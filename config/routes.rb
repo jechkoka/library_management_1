1a72f36e4eaefe131bce49257c22cf02a7376cc6
jechkoka <73609108+jechkoka@users.noreply.github.com>
2025-08-29 08:23:33 +0000

    FEAT/DB,modèles,Controleurs,routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  root 'books#index'
+  
+  resources :books
+  resources :users
+  resources :loans
+  
+  # Route pour marquer un livre comme retourné
+  patch '/loans/:id/return', to: 'loans#return_book', as: 'return_book'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
