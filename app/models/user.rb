d0a3241debf86c494fa3cf8b0590c96c97c52290
jechkoka <73609108+jechkoka@users.noreply.github.com>
2025-08-29 10:04:19 +0000

    FEAT/app/models/user.rb/firstname lastname update

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ApplicationRecord
+  has_many :loans
+  has_many :books, through: :loans
+  
+  validates :firstname, :lastname, :email, presence: true
+  validates :email, uniqueness: true
+  
+  # Méthode pour obtenir le nom complet
+  def full_name
+    "#{firstname} #{lastname}"
+  end
 end
